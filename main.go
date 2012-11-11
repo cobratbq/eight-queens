6b7e72eb5ff2dace6c8c0fad18579c8ede5757a2
Danny van Heumen <danny@dannyvanheumen.nl>
2012-11-11 22:27:38 +0100

    Ability to specify problem size. For size n: board size nxn, n queens to be positioned.

--- main.go
+++ main.go
@@ -12,30 +12,31 @@ const (
 )
 
 func main() {
-	Solve()
+	Solve(12)
 }
 
-func Solve() {
+func Solve(size int) {
 	solution := make([]uint8, 0)
-	constraints := [8]uint8{UNUSED, UNUSED, UNUSED, UNUSED, UNUSED, UNUSED, UNUSED, UNUSED}
-	solve(solution, constraints)
+	constraints := make([]uint8, size)
+	solve(size, solution, constraints)
 }
 
-func solve(solution []uint8, constraints [8]uint8) {
+func solve(size int, solution []uint8, constraints []uint8) {
 
-	if len(solution) >= 8 {
+	if len(solution) >= size {
 		//base case: all queens placed, we're done
 		fmt.Printf("Solution found: %v\n", solution)
 		return
 	}
 
-	nextConstr, potentials := prepareNext(&constraints)
+	nextConstr, potentials := prepareNext(size, constraints)
 
 	//check potential queen positions
 	for _, pos := range potentials {
 
 		//copy constraints for next step
-		tryConstr := nextConstr
+		tryConstr := make([]uint8, 0, size)
+		tryConstr = append(tryConstr, nextConstr...)
 
 		//copy current solution for further exploration in next step
 		trySolution := make([]uint8, 0, len(solution)+1)
@@ -47,18 +48,19 @@ func solve(solution []uint8, constraints [8]uint8) {
 		if pos > 0 {
 			tryConstr[pos-1] |= DECREASING
 		}
-		if pos < 7 {
+		if pos < uint8(size-1) {
 			tryConstr[pos+1] |= INCREASING
 		}
 
 		//try to solve with current position
-		solve(trySolution, tryConstr)
+		solve(size, trySolution, tryConstr)
 	}
 
 	return
 }
 
-func prepareNext(currentConstr *[8]uint8) (nextConstr [8]uint8, potentials []uint8) {
+func prepareNext(size int, currentConstr []uint8) (nextConstr []uint8, potentials []uint8) {
+	nextConstr = make([]uint8, size)
 	potentials = make([]uint8, 0)
 
 	for i, constraint := range currentConstr {
@@ -73,7 +75,7 @@ func prepareNext(currentConstr *[8]uint8) (nextConstr [8]uint8, potentials []uin
 				//mark position for prior queen's vertical influence
 				nextConstr[i] |= CONSTANT
 			}
-			if i < 7 && constraint&INCREASING == INCREASING {
+			if i < size-1 && constraint&INCREASING == INCREASING {
 				//mark position for prior queen's forward diagonal influence
 				nextConstr[i+1] |= INCREASING
 			}
